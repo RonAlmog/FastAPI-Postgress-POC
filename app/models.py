29443abbc738f399c14a968c69320aaec0ee0ff5
ron-afs <94569277+ron-afs@users.noreply.github.com>
2022-03-29 15:24:01 -0400

    add alembic migrations

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ class Post(Base):
     __tablename__ = "posts"
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
+    slug = Column(String, nullable=True)
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
